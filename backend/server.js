229806ffd949009b0256f5aa3c0bc6331c173d24
pragatiagrawal69 <your.email@example.com>
2026-07-17 19:01:40 +0530

    Setup Express, Nodemon, Mongoose, and JWT

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+console.log("Pragati");
+import express from "express"
+
+const app = express()
+const PORT = 5555
+
+app.listen(PORT, () => {
+    console.log(`Server is listening at port ${PORT}`)
+})
