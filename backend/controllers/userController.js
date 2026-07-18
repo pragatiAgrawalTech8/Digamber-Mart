3c13b1383ab87a58f0cad68fa8387f12283dc36a
pragati8-tech <prag90987@gmail.com>
2026-07-18 19:33:03 +0530

    configure Nodemailer for email verification

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,27 +1,34 @@
 
-import {User} from "../models/userModel.js"
+import { User } from "../models/userModel.js"
 import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
+import { verifyEmail } from "../emailVerify/verifyemail.js";
 
-export const register = async(req,res)=>{
+export const register = async (req, res) => {
     try {
-        const  {firstName,lastName,email,password} = req.body
-        if(!firstName || !lastName || !email || !password){
-            res.status(400).json({success:false,message:'All fields are required'})
+        const { firstName, lastName, email, password } = req.body
+        if (!firstName || !lastName || !email || !password) {
+            return res.status(400).json({ success: false, message: 'All fields are required' })
         }
-        const user = await User.findOne({email}) 
-        if(user){
-            res.status(400).json({success:false,message:'user already exists'})
+        const user = await User.findOne({ email })
+        if (user) {
+            res.status(400).json({ success: false, message: 'user already exists' })
         }
-        const hashedPassword = await bcrypt.hash(password,10)
+        const hashedPassword = await bcrypt.hash(password, 10)
+
         const newUser = await User.create({
             firstName,
             lastName,
             email,
-            password : hashedPassword
+            password: hashedPassword
         })
+
+        const token = jwt.sign({ id: newUser._id }, process.env.SECRET_KEY, { expiresIn: '10m' })
+        verifyEmail(token, email) // send email here
+        newUser.token = token
         await newUser.save()
-        return res.status(201).json({success:true,message:'user registered successfully',user:newUser})
+        return res.status(201).json({ success: true, message: 'user registered successfully', user: newUser })
     } catch (error) {
-        res.status(500).json({success:false,message:error.message})
+        res.status(500).json({ success: false, message: error.message })
     }
 }
