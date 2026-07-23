ba3a43341ec53b0222c9bb989928c070b6d28460
pragati8-tech <prag90987@gmail.com>
2026-07-23 17:16:32 +0530

    Setup React frontend with Tailwind CSS and shadcn/ui

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Button} from "./components/ui/button"
+const App = () => {
+  return (
+    <>
+    <div class="text-amber-200">App</div>
+    <Button>Click Me</Button>
+    <button>Click Me</button>
+    </>
+  )
+}
+
+export default App
