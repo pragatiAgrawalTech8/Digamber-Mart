d83d7704226af790baf367d8964ae6805b250d2d
pragati8-tech <prag90987@gmail.com>
2026-07-24 19:53:43 +0530

    add signup pages

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ console.log("Pragati");
 import express from "express"
 import connectDB from "./database/db.js"
 import "dotenv/config"
-
+import cors from "cors"
 import userRoute from "./routes/userRoute.js"
 
 const app = express()
@@ -10,7 +10,10 @@ const PORT = process.env.PORT || 5555
 
 //middleware
 app.use(express.json())
-
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 
 app.use("/api/v1/user",userRoute)
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import {Button} from "./components/ui/button"
+import {createBrowserRouter,RouterProvider} from "react-router-dom"
+import NavBar from "./components/NavBar"
+import Home from "./pages/Home"
+import Signup from "./pages/Signup"
+import Login from "./pages/Login"
+const router = createBrowserRouter([
+{
+  path:'/',
+  element:<><NavBar/><Home/></>
+},
+{
+  path:'/signup',
+  element:<><Signup/></>
+},
+{
+  path:'/login',
+  element:<><Login/></>
+}
+])
+
 const App = () => {
   return (
     <>
-    <div class="text-amber-200">App</div>
-    <Button>Click Me</Button>
-    <button>Click Me</button>
+   <RouterProvider router={router}/>
     </>
   )
 }
