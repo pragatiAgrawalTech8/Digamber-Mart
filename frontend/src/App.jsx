897c7ea4561917f71e5a091d54845b09b3e4f11b
pragati8-tech <prag90987@gmail.com>
2026-07-25 11:21:36 +0530

    add email verification flow and verification status pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import NavBar from "./components/NavBar"
 import Home from "./pages/Home"
 import Signup from "./pages/Signup"
 import Login from "./pages/Login"
+import Verify from "./pages/Verify"
+import VerifyEmail from "./pages/VerifyEmail"
 const router = createBrowserRouter([
 {
   path:'/',
@@ -17,6 +19,14 @@ const router = createBrowserRouter([
 {
   path:'/login',
   element:<><Login/></>
+},
+{
+  path:'/verify',
+  element:<><Verify/></>
+},
+{
+  path:'/verify/:token',
+  element:<><VerifyEmail/></>
 }
 ])
 
